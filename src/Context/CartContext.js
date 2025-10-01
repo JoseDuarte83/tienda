8e4c08027b2507dedd689bc184d76239c65402ac
JoseDuarte83 <joseduartesls@hotmail.com>
2025-09-30 20:07:03 -0600

    completo el componente ProductCard

--- src/Context/CartContext.js
+++ src/Context/CartContext.js
@@ -1,4 +1,4 @@
-import React { createContext, useContext, useReducer } from "react";
+import React, { createContext, useContext, useReducer} from "react";
 
 //crear context
 
@@ -16,6 +16,76 @@ const initialState = {
 
 const cartReducer = (state, action)=>{
     switch (action.type){
-        case
+        case 'ADD_TO_CART':
+            const existingItem = state.items.find(item => item.id === action.payload.id);
+
+            if(existingItem){
+                const updatedItems = state.items.map(item => 
+                    item.id === action.payload.id 
+                    ? {...item, quantity: item.quantity +1 } 
+                    : item
+                );
+            return {
+                ...state,
+                items: newItem,
+                total: calculateTotal(updatedItems)
+             };
+            }
+            const newItems = [...state.items, {...action.payload, quantity: 1}];
+            return {
+                ...state,
+                items: newItem,
+                total: calculateTotal(newItems),
+            };
+        case 'REMOVE_FROM_CART':
+            const filteredItems = state.items.filter(item => item.id !== action.payload);
+            return {
+                ...state,
+                items: filteredItems, 
+                total: calculateTotal(filteredItems),
+            };
+        case 'UPDATE_QUANTITY':
+            const updatedItems = state.items.map((item) =>
+            item.id === action.payload.id
+            ? { ...item, quantity: action.payload.quantity }
+            : item
+        ).filter(item => item.quantity > 0);
+        return {
+            ...state,
+            items: updatedItems,
+            total: calculateTotal(updatedItems)
+        };
+        case 'TOGGLE_CART':
+            return {
+                ...state,
+                isOpen: !state.isOpen
+            };
+        case 'CLEAR_CART':
+            return initialState;
+        default: 
+            return state;
     }
-}
+};
+
+const calculateTotal = (items)=>{
+    return items.reduce((total,item)=> total + (item.price * item.quantity),0);
+};
+
+// provider de context
+
+export const CartProvider = ({children})=>{
+    const [ state, dispatch ] = useReducer(cartReducer, initialState);
+
+    return (
+        <CartContext.Provider value={{ state, dispatch }}>{children}</CartContext.Provider>
+    );
+};
+// crear un hook personalizado
+
+export const useCart = ()=>{
+    const context = useContext(CartContext)
+    if (!context){
+        throw new Error('useCart debe de ser usado dentro de CartProvider');
+    }
+    return context;
+};
---
+++ src/components/Product/ProductCard.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import { Card, CardMedia, CardContent,CardActions,Typography,Box, Rating } from "@mui/material";
+import { AddShoppingCart } from "@mui/icons-material";
+import { useCart } from 
